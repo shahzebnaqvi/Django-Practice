df6cd29015acc255e69f52b336b8ec636453298c
unknown <shahzeb.naqvi12@gmail.com>
2022-01-26 02:37:05 +0500

    class 2 changes

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import  admin
 from django.urls import include,path
 
 urlpatterns = [
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('contact/', include('contact.urls')),
 
     path('admin/', admin.site.urls),
