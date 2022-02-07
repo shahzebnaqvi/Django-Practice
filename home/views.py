2c94a6ca388e7eae1e0fd823b6b38a68be517b32
unknown <shahzeb.naqvi12@gmail.com>
2022-02-07 23:03:07 +0500

    useradded

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the home index.")
+    return HttpResponse("Hello, world.")
 
 
 def homepage(request):
