4346ce3da49e7cedf93e2ca90e511a3f34c49ac4
unknown <shahzeb.naqvi12@gmail.com>
2022-02-07 23:30:54 +0500

    useradded

--- contact/models.py
+++ contact/models.py
@@ -12,11 +12,9 @@ class Todo(models.Model):
         return self.title
         # signup table
 class Signup(models.Model):
-    title = models.CharField(max_length = 100)
-    body = models.CharField(max_length = 100)
-    is_completed = models.BooleanField(default=False)
-    date_created = models.DateField(auto_created=True)
-    last_modified = models.DateField(auto_now=True)
-
+    number = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100)
+    email = models.EmailField(max_length = 100)
+  
     def __str___(self):
         return self.title
